f346786a014953f7bb60174883941276bcfe83a2
akurnya <akurnya@gmail.com>
2016-04-30 09:48:38 +0000

    added search place feature on index page. added landing.js get search Location function. some minimal landing css

--- src/_modules/landing/landing.js
+++ src/_modules/landing/landing.js
@@ -1,8 +1,51 @@
 'use strict';
 
-let Landing = function Landing() {
-  this.name = 'landing';
-  console.log('%s module', this.name);
-};
+let searchLoc = (function () {
+  let
+    Landing = function Landing() {
+      console.log('%s module loaded', 'landing');
+    },
 
-export default Landing;
+    showPosition = function showPosition(position) {
+      return [position.coords.latitude, position.coords.longitude];
+    },
+    showError =
+    function showError(error) {
+      let res;
+      switch(error.code) {
+      case error.PERMISSION_DENIED:
+        res = 'User denied the request for Geolocation.';
+        break;
+      case error.POSITION_UNAVAILABLE:
+        res = 'Location information is unavailable.';
+        break;
+      case error.TIMEOUT:
+        res = 'The request to get user location timed out.';
+        break;
+      case error.UNKNOWN_ERROR:
+        res = 'An unknown error occurred.';
+        break;
+      }
+      return res;
+    },
+    getLocation = function getLocation() {
+      return new Promise(function (resolve, reject) {
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition( (pos) => {
+            resolve(showPosition(pos));
+          },
+          (err) => {
+            reject(Error(showError(err)));
+          }, {enableHighAccuracy:true});
+        }
+        else {
+          reject(Error('Geolocation is not supported by this browser.'));
+        }
+      });
+    };
+
+  Landing();
+  return getLocation;
+})();
+
+export default searchLoc;
--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -5,8 +5,19 @@
 
 import $ from 'jquery';
 import Link from '../_modules/link/link';
+import searchLoc from '../_modules/landing/landing';
 
 $(() => {
   new Link(); // Activate Link modules logic
-  console.log('Welcome to Yeogurt!');
+  document.getElementById('searchgo').addEventListener('click', () => {
+    searchLoc()
+    .then((val) => {
+      let [lat, lng] = val;
+      console.log(`lat ${lat} lng ${lng}` );
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  });
+  console.log('All systems green');
 });
