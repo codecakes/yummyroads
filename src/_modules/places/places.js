83b489a99fe5c898c32a1c2f566c929042da9ecd
akurnya <akurnya@gmail.com>
2016-05-03 15:15:32 +0000

    working on slider panel that will contain div cards of results

--- src/_modules/places/places.js
+++ src/_modules/places/places.js
@@ -71,27 +71,37 @@ const
       fragment = document.createDocumentFragment(),
       ulFrag = document.createElement('ul'),
       responseResults = document.getElementsByClassName('responseResults')[0];
-    let div, li, h3, img, p, photo, tmp;
+    let div, li, h3, logo, logodiv, p, photo, tmp, photodiv;
 
     responseArr.forEach((obj) => {
       // console.log(obj);
       h3 = document.createElement('h3');
-      img = document.createElement('img');
-      photo = document.createElement('img');
+      logo = document.createElement('img');
       p = document.createElement('p');
       li = document.createElement('li');
       div = document.createElement('div');
+      logodiv = document.createElement('div');
 
       h3.textContent = obj.name;
       div.appendChild(h3);
-      img.src = obj.icon;
-      div.appendChild(img);
+      logo.src = obj.icon;
+
+      logodiv.className += ' logodiv';
+      logodiv.appendChild(logo);
+      div.appendChild(logodiv);
 
       if (obj.photos) {
+        photo = document.createElement('img');
         // console.log(tmp[0]);
         tmp = obj.photos[0];
-        photo.src = tmp.getUrl({maxHeight: 300, maxWidth: 300});
-        div.appendChild(photo);
+        photo.width = 200;
+        photo.height = 200;
+        photo.src = tmp.getUrl({maxHeight: 200, maxWidth: 200});
+        photodiv = document.createElement('div');
+        photodiv.className += ' photodiv';
+        photodiv.appendChild(photo);
+        photodiv.id = 'imgdiv';
+        div.appendChild(photodiv);
       }
 
       if (obj.price_level) {
@@ -104,8 +114,48 @@ const
     });
 
     ulFrag.appendChild(fragment);
-
+    ulFrag.className+=' w3-ul w3-card-4 w3-center';
+    let divs = ulFrag.querySelectorAll('li > div');
+    for (let i = 0,ln = divs.length; i<ln; i++) {
+      divs[i].className += ' w3-container w3-center w3-hover-green';
+      divs[i].id = 'search-result';
+      divs[i].querySelector('h3').className += ' w3-red';
+    }
+    responseResults.className += ' w3-card';
     responseResults.appendChild(ulFrag);
+
+  },
+  createSlider = (responseArr) => {
+    let
+      fragment = document.createDocumentFragment(),
+      spanSlider, img, divImgHolder, btnHolder, btnHref,
+      divFrag = parseResult(responseArr),
+      ln = divFrag.children.length,
+      divImgHolder = document.createElement('div'),
+      responseResults = document.getElementsByClassName('responseResults')[0];
+
+    responseResults.className += ' w3-card';
+    divImgHolder.className += ' div-img-holder';
+
+    for (let i=0, idName, childNodes = divFrag.children; i<ln; i++) {
+      spanSlider = document.createElement('span');
+      idName = 'slider-'+'div-'+i;
+      spanSlider.id = idName;
+      responseResults.appendChild(spanSlider);
+
+      divImgHolder.appendChild(childNodes[i]);
+
+      btnHref = document.createElement('a');
+      btnHref.href = '#'+idName;
+      btnHref.className += 'slider-change';
+      btnHolder.appendChild(btnHref);
+    }
+
+    responseResults.appendChild(divImgHolder);
+    responseResults.appendChild(btnHolder);
+
+    // TODO: Fix the width of divImgHolder by calculating total div cards
+    // 3 div cards per slide
   };
 
 export default {getPlaces, parseResult};
